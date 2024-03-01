e1e6249042d0f59863df4a226a12700eb71bc0f8
baselkelziye <baselkelziye@gmail.com>
2024-03-01 14:46:19 +0300

    delete unnecessary checkpoints

--- mujo.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-filename = "/home/basel/PycharmProjects/pythonProject/checkpoints/mountaincarcontinuous/policies/default_policy/policy_state.pkl"
-
-with open(filename, 'rb') as file:
-   data = pickle.load(file)
-
-
-print(data)
