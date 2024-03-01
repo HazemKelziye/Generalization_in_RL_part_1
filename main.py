b61463925307b375be5fd55bbed2ca54a03a4d16
baselkelziye <baselkelziye@gmail.com>
2024-03-01 15:01:11 +0300

    last modifications

--- main.py
+++ main.py
@@ -16,23 +16,23 @@ ppo_config = ppo_config.framework("torch")
 ppo_config = ppo_config.training(model={"fcnet_hiddens": [256, 256]},
                                  lr=0.0003, kl_coeff=0.3, clip_param=0.2, lambda_=0.95,
                                  gamma=0.99, train_batch_size=5000)
-ppo_config = ppo_config.environment(env="CartPole-v1")
+ppo_config = ppo_config.environment(env="Acrobot-v1")
 ppo_config = ppo_config.resources(num_gpus=1, num_cpus_for_local_worker=2,
                                   num_gpus_per_worker=0.5, num_cpus_per_worker=2)
 ppo_config = ppo_config.rollouts(num_rollout_workers=2)
 
 agent = ppo_config.build()
-agent.restore("/home/basel/PycharmProjects/pythonProject/checkpoints/cartpole1")
+agent.restore("/home/basel/PycharmProjects/pythonProject/checkpoints/checkpoints6")
 print(PPO.default_resource_request(ppo_config))
 
 
 
 
 ### Training Loops
-for iterat in range(51):
+for iterat in range(11):
 
     ###### Evaluation through the environment
-    env = gym.make("CartPole-v1", render_mode="human")
+    env = gym.make("Acrobot-v1", render_mode="human")
     episode_reward = 0
     terminated = truncated = False
     obs, _ = env.reset()
@@ -50,6 +50,6 @@ for iterat in range(51):
     # print(f"number of agent steps trained: {result['num_agent_steps_trained']}")
     # print(f"GPU system monitoring: {GPUtil.showUtilization()}")
     # if iterat % 25 == 0:
-    #     checkpoint_path = agent.save("/home/basel/PycharmProjects/pythonProject/checkpoints/cartpole1")
+    #     checkpoint_path = agent.save("/home/basel/PycharmProjects/pythonProject/checkpoints6")
 
 ray.shutdown()
